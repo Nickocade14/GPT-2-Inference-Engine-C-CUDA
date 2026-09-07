6cfbb63472009be86dadad9e14ac2f01191f87f7
Nicolas Cadena <Nickocade15>
2026-09-07 17:28:59 -0400

    Add CUDA memory allocation and host-to-device transfers for matmul

---
+++ src/matmul_gpu.cu
@@ -0,0 +1,49 @@
+#include <cstdio> 
+void matrixA_Filled(float *matrixA, int M, int K){
+    for(int i = 0; i < M*K; i++){
+        matrixA[i] = float(i + 1);
+    }
+}
+void matrixB_Filled(float *matrixB, int K, int N){
+    for(int i = 0; i < K*N; i++){
+        matrixB[i] = float(i + 1);
+    }
+}
+int main(){
+    /* 
+    Matrix A = (M, K) 
+    Matrix B = (K, N) 
+    Matrix C = (M, N) 
+    */ 
+
+    int M = 3; 
+    int K = 4; 
+    int N = 2; 
+    
+    // Device Varibles
+    float *d_A;
+    float *d_B;
+    float *d_C;
+
+    cudaMalloc(&d_A, (M * K) *sizeof(float));
+    cudaMalloc(&d_B, (K * N) *sizeof(float));
+    cudaMalloc(&d_C, (M * N) *sizeof(float));
+
+    //Host Variables 
+    float *test = new float[M * K];
+    float *matrixA = new float[M * K];
+    float *matrixB = new float[K * N];
+    float *matrixC = new float[M * N];
+
+    matrixA_Filled(matrixA, M, K);
+    matrixB_Filled(matrixB, K, N);
+
+    cudaMemcpy(d_A, matrixA, (M*K) * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, matrixB, (K*N) * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaMemcpy(test, d_A, (M*K) * sizeof(float), cudaMemcpyDeviceToHost);
+
+    for(int i = 0; i< M*K; i++){
+        printf("%f ", test[i]);
+    }
+}
